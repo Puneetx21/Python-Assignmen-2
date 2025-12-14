cecf77bbbb51a59e04915beceb15b48e291664d3
Puneetx21 <punitchauhan2103@gmail.com>
2025-12-14 20:59:33 +0530

    task 2 done

---
+++ task2.py
@@ -0,0 +1,8 @@
+# Print numbers from 1 to 50 and sum of all this numbers
+
+
+sum_total = 0
+for i in range(1,51):
+    sum_total = sum_total + i
+    print(f"The total sum of numbers are : {sum_total}")
+    # print(i)
