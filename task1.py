1b7a32da121df3ba5a48a0d2c69b3ee84c795fa3
Puneetx21 <punitchauhan2103@gmail.com>
2025-12-14 20:42:49 +0530

    task 1 done

---
+++ task1.py
@@ -0,0 +1,8 @@
+# TO CHECK WHETHER A NUMBER IS EVEN OR ODD
+
+num = int(input("Enter a number : "))
+
+if num % 2 == 0:
+    print("The Entered number is Even.")
+else:
+    print("The Entered number is Odd.")
