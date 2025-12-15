094f7c6ca829317a8fd04994407b9a344b699ab5
Puneetx21 <punitchauhan2103@gmail.com>
2025-12-15 14:04:28 +0530

    corrected logic

--- task2.py
+++ task2.py
@@ -4,5 +4,5 @@
 sum_total = 0
 for i in range(1,51):
     sum_total = sum_total + i
-    print(f"The total sum of numbers are : {sum_total}")
-    # print(i)
+
+print(f"The total sum of numbers are : {sum_total}")
